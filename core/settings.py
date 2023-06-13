8e932ff5295a36505a9b40b8189466e549bc7b60
Michael <mikhzobby@gmail.com>
2023-06-13 13:31:20 +0100

    New slate

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +134,12 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Email Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- results/utils.py
+++ results/utils.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
+from django.core.mail import EmailMessage, send_mail
+from django.conf import settings
 
 from xhtml2pdf import pisa
 
@@ -10,5 +12,6 @@ def render_to_pdf(template_src, context_dict={}):
     output = BytesIO()
     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), output)
     if not pdf.err:
+        send_mail("Hello", "Hope you are good?", from_email=settings.EMAIL_HOST_USER, recipient_list=['mikhzobby@gmail.com'])
         return HttpResponse(output.getvalue(), content_type='application/pdf')
     return None
